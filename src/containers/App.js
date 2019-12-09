3610c885acf876d7512f3df20ab088133510fd10
danilo <d.morya@speedflow.com>
2019-12-09 16:36:22 +0200

    fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -34,7 +34,7 @@ import AddOrEditApartment from '../components/AddOrEditApartment';
     return (
       <Container>
         <Row>
-          <AddOrEditApartment key={Math.random*1000} store={this.props.store} />
+          <AddOrEditApartment store={this.props.store} />
         </Row>
         <br></br>
         <Row>
